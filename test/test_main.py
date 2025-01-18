39948c16d8db94f93e7c311fbf8faf2c17932b7a
shafeeh011 <shafeeh011@.com>
2025-01-18 21:50:12 +0530

    checking added new fun and test cases

--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,11 @@
-from src.main import add
+from src.main import add, subtract
 
 def test_add():
     assert add(1, 2) == 3
     assert add(2, 2) == 4
     assert add(3, 2) == 5
 
+def test_subtract():
+    assert subtract(1, 2) == -1 
+    assert subtract(2, 2) == 0
+    assert subtract(3, 2) == 1
